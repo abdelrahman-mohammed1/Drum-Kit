a37d397e68fed6c50e01ce582caaafdde5410f6d
Abdelrahman Mohamed Mohamed Ahmed <75761246+abdelrahman-mohammed1@users.noreply.github.com>
2024-06-30 01:18:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,31 +16,31 @@ function makeSound(key) {
   let sound;
   switch (key) {
     case "w":
-      sound = new Audio("/sounds/tom-1.mp3");
+      sound = new Audio("./sounds/tom-1.mp3");
       sound.play();
       break;
     case "a":
-      sound = new Audio("/sounds/tom-2.mp3");
+      sound = new Audio("./sounds/tom-2.mp3");
       sound.play();
       break;
     case "s":
-      sound = new Audio("/sounds/tom-3.mp3");
+      sound = new Audio("./sounds/tom-3.mp3");
       sound.play();
       break;
     case "d":
-      sound = new Audio("/sounds/tom-4.mp3");
+      sound = new Audio("./sounds/tom-4.mp3");
       sound.play();
       break;
     case "j":
-      sound = new Audio("/sounds/snare.mp3");
+      sound = new Audio("./sounds/snare.mp3");
       sound.play();
       break;
     case "k":
-      sound = new Audio("/sounds/crash.mp3");
+      sound = new Audio("./sounds/crash.mp3");
       sound.play();
       break;
     case "l":
-      sound = new Audio("/sounds/kick-bass.mp3");
+      sound = new Audio("./sounds/kick-bass.mp3");
       sound.play();
       break;
     default:
